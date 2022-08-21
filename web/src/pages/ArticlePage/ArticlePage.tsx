00a3d0e47569684e5283cbade9fd07351d53d5e7
bacale <bacale86@gmail.com>
2022-08-21 21:31:36 +0100

    crud in rw js

---
+++ web/src/pages/ArticlePage/ArticlePage.tsx
@@ -0,0 +1,19 @@
+import { MetaTags } from '@redwoodjs/web'
+
+import ArticleCell from 'src/components/ArticleCell'
+
+type Props = {
+  id: number
+}
+
+const ArticlePage = ({ id }: Props) => {
+  return (
+    <>
+      <MetaTags title="Article" description="Article page" />
+
+      <ArticleCell id={id} />
+    </>
+  )
+}
+
+export default ArticlePage
